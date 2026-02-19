c2e1ba216534996e49faf595d28b62f84f65af62
07princeff-bit <07princeff@gmail.com>
2026-02-19 17:04:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,27 @@
-let player = document.getElementById("player");
-let leftBtn = document.getElementById("leftBtn");
-let rightBtn = document.getElementById("rightBtn");
-let jumpBtn = document.getElementById("jumpBtn");
+function loginUser(){
+let user = document.getElementById("username").value;
+let pass = document.getElementById("password").value;
 
-let x = 50;
-let y = 60;
-let velocity = 0;
-let gravity = 0.6;
-let isJumping = false;
-
-function update(){
-    velocity -= gravity;
-    y += velocity;
-
-    if(y <= 60){
-        y = 60;
-        isJumping = false;
-    }
-
-    player.style.bottom = y + "px";
-    player.style.left = x + "px";
-
-    requestAnimationFrame(update);
+if(user==="admin" && pass==="1234"){
+window.location="dashboard.html";
+return false;
+}else{
+alert("Wrong Login!");
+return false;
 }
-update();
+}
 
-// Keyboard Controls
-document.addEventListener("keydown", e=>{
-    if(e.key === "ArrowLeft") x -= 10;
-    if(e.key === "ArrowRight") x += 10;
-    if(e.key === "ArrowUp" && !isJumping){
-        velocity = 12;
-        isJumping = true;
-    }
+function showSection(id){
+document.querySelectorAll(".section").forEach(sec=>{
+sec.style.display="none";
 });
+document.getElementById(id).style.display="block";
+}
 
-// 📱 Touch Controls
-leftBtn.addEventListener("touchstart", ()=> x -= 20);
-rightBtn.addEventListener("touchstart", ()=> x += 20);
-jumpBtn.addEventListener("touchstart", ()=>{
-    if(!isJumping){
-        velocity = 12;
-        isJumping = true;
-    }
-});
+function toggleDark(){
+document.body.classList.toggle("dark");
+}
+
+function logout(){
+window.location="index.html";
+}
